577d736158c3c50532f91e579dc1ae284d1d7bc7
Iván Martearena <ivanofkrukoft@hotmail.com>
2022-10-19 05:07:24 -0300

    agregado agustin2

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
     showDialog(
         context: context,
         builder: (context) {
-          return WorkCalendarComponent();
+          return WorkCalendarComponent(task);
         });
   }
 
