b2121e3f7bc6fdb1cb9f6e67734b85a27543fc69
Iván Martearena <ivanofkrukoft@hotmail.com>
2022-10-19 02:16:37 -0300

    maquetado

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const WorkCalendarComponent(),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  void _save() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return WorkCalendarComponent();
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: MaterialButton(
+            onPressed: _save,
+            padding: const EdgeInsets.all(12),
+            color: Colors.blueAccent,
+            child: Text("trabajos"),
+          ),
+        ),
+      ) // This trailing comma makes auto-formatting nicer for build methods.
+    ;
+  }
+}
--- lib/src/work_calendar_component.dart
+++ lib/src/work_calendar_component.dart
@@ -8,8 +8,82 @@ class WorkCalendarComponent extends StatefulWidget {
 }
 
 class _WorkCalendarComponentState extends State<WorkCalendarComponent> {
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return _workCalendar();
   }
 }
+
+
+_workCalendar(){
+  return AlertDialog(
+    insetPadding: EdgeInsets.fromLTRB(5, 200, 5, 200),
+    elevation: 0,
+    backgroundColor: Colors.teal,
+    content: (_worckCalendarCard()),
+  );
+}
+
+_worckCalendarCard(){
+  return Padding(
+    padding: const EdgeInsets.all(0.1) ,
+    child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          height: 400,
+          color: Colors.amberAccent,
+          child: Column(
+            children: [
+              Text("persona"),
+
+              const Divider(
+                color: Colors.grey,
+                thickness: 3,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.indigo,
+                      )),
+
+                  Expanded(
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        color: Colors.redAccent,
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: double.infinity,
+                              color: Colors.cyanAccent,
+                            ),
+                            Container(
+                              height: 50,
+                              width: double.infinity,
+                              color: Colors.black,
+                            )
+
+                          ],
+
+                        ),
+                      ))
+                ],
+              )
+            ],
+
+          ),
+        )
+
+
+    ),
+  );
+}
+
+
