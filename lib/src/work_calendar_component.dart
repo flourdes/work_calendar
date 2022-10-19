e99185c65a349203f301c3b3f9101e5c966b1564
Iván Martearena <ivanofkrukoft@hotmail.com>
2022-10-19 03:49:44 -0300

    agregado lourdes

--- lib/src/work_calendar_component.dart
+++ lib/src/work_calendar_component.dart
@@ -86,3 +86,41 @@ _worckCalendarCard(){
     ),
   );
 }
+
+
+Widget _profits(){
+  late int ganancia = 555;
+  late int ganancias = ganancia + 1000;
+  return Container(
+    height: 200,
+    width: double.infinity,
+    color: Colors.black,
+    child: Column(
+      children: [
+        _profitsContainer('Ganancia Diaria:', ganancia),
+        const SizedBox(height: 20,),
+        _profitsContainer('Ganancia Total:', ganancias),
+      ],
+    ),
+  );
+}
+Widget _profitsContainer(String label, ganancia){
+  return Container(
+    padding: const EdgeInsets.all(10),
+    height: 50,
+    decoration: BoxDecoration(
+      color: Colors.grey.shade200,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label, style: const TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.w200,)),
+        const Expanded(child: SizedBox.shrink()),
+        const Icon(Icons.attach_money, size: 20, color: Colors.teal,),
+        Text(ganancia.toString(), style: const TextStyle(fontSize: 20, color: Colors.teal,),),
+      ],
+    ),
+  );
+}
